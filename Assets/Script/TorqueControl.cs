3c1cf2a6ddb9735f0dbc7a1829a44a2e3fccf7d5
Naranat-Seeaon <naranat.seea@bumail.net>
2026-03-26 16:16:26 +0700

    week11 MagnusEffect

---
+++ Assets/Script/TorqueControl.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ToqueControl : MonoBehaviour
+{
+
+    public float torquePower = 1f;
+    private Rigidbody _rb;
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+
+    void FixedUpdate()
+    {
+        if (Keyboard.current.dKey.isPressed)
+        {
+            _rb.AddRelativeTorque (0,0,torquePower);
+        }
+
+    }
+}
